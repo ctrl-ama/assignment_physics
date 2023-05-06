c2bd7cfef82096d7024ac02f7ec1ee51b6188bbc
bOLA-532 <faithadebolaibrahim@gmail.com>
2023-05-05 20:45:24 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create scanner object for user input
+        Scanner input = new Scanner(System.in);
+
+        // Prompt user for input
+        System.out.print("Enter the mass of the object in kg: ");
+        double m = input.nextDouble();
+
+        System.out.print("Enter the height of the object in meters: ");
+        double h = input.nextDouble();
+
+        // Calculate potential energy using the PotentialEnergy class
+        double PE = PotentialEnergy.calculatePotentialEnergy(m, h);
+
+        // Output result
+        System.out.println("The potential energy of the object is " + PE + " J");
+    }
+}
---
+++ src/PotentialEnergy.java
@@ -0,0 +1,8 @@
+// Define the PotentialEnergy class
+public class PotentialEnergy {
+    // Define the calculatePotentialEnergy method
+    public static double calculatePotentialEnergy(double m, double h) {
+        double g = 9.81;
+        return m * g * h;
+    }
+}
