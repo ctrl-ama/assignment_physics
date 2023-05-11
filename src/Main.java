b214e419e627283b7d41e85f19d2318dff73100c
Amayindi Yerima <amayindi.yerima@binghamuni.edu.ng>
2023-05-11 14:34:18 +0100

    added calculateForce

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        Gforce calculator = new Gforce();
+        //prompt for the user
+        System.out.print("Enter your mass in kg:");
+        double m = input.nextDouble();
+        System.out.print("The force is" + calculator.calculateForce(m));
+
+        //calculate force using field.java
+
+
+//        double Force = Gforce.calculateForce(float mass,double gravity){
+//        float calculateForce(float mass,double gravity)
+
+        // output result
+
+//        System.out.print("The force is" + Force+"N");
+
+
+
+
+
+
 
 
     }
