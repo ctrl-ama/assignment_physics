8bbd26be6a90ca90878020457ee5b7170d93fd28
[tishe Adeniyi] <[oluwatishe.adeniyi@binghamuni.edu.ng]>
2023-05-12 17:57:23 +0100

    Tishe's first commit

--- src/UI/Gforce.java
+++ src/UI/Gforce.java
@@ -2,17 +2,22 @@ package UI;
 
 public class Gforce {
 
-        public static double calculateForce(double m, double v){
-            double g= 9.81;
-            return (float) (m * g);
-        }
+    public static double calculateForce(double m, double v) {
+        double g = 9.81;
+        return (float) (m * g);
+    }
 
     // Code to cALc Potential Energy by bola
-        
+
     // Define the calculatePotentialEnergy method.
     public static double calculatePotentialEnergy(double m, double h) {
         double g = 9.81;
         return m * g * h;
+    }
+
+    static float calculatekineticEnergy(float m, float v) {
+        float k = 0.5f;
+        return m * v*k;
     }
 }
 
